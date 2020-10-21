527c0cb19ef5d0c42e5ad1000a820142141dd534
Karan Bhamra <karan.bhamra@gmail.com>
2020-10-20 22:53:00 -0700

    Removed some explicit casts and added comments

--- Raycast/Form1.cs
+++ Raycast/Form1.cs
@@ -17,8 +17,9 @@ namespace Raycast
         private Random gen;
         private List<Line> barriers;
         private const float end = (float)(Math.PI * 2);
-        private const float step = 0.1f;
+        private const float angleStep = 0.1f;
         private const int rayScaleDist = 5000;
+        private const int numBarriers = 5;
         private int minIntersectionDistance;
         private Point mouse;
         private float currentAngle;
@@ -36,6 +37,7 @@ namespace Raycast
             gfx = Graphics.FromImage(canvas);
             gen = new Random();
 
+            // Add borders of the form.
             barriers = new List<Line>()
             {
                 new Line(0,1,canvas.Width, 1), // top
@@ -44,25 +46,27 @@ namespace Raycast
                 new Line(pictureBox1.Width - 1, 0, pictureBox1.Width - 1, pictureBox1.Height)  // right
             };
 
-            for (int i = 0; i < 5; i++)
+            // Add the random lines that will act as additional barriers.
+            for (int i = 0; i < numBarriers; i++)
             {
                 int startX = gen.Next(0, canvas.Width);
                 int endX = gen.Next(0, canvas.Width);
-
                 int startY = gen.Next(0, canvas.Height);
                 int endY = gen.Next(0, canvas.Height);
                 barriers.Add(new Line(startX, startY, endX, endY));
             }
-
         }
 
+        // Cast rays around the circle going from 0 to 2 pi.
+        // Check for collision, and the ray's end point will be the closest intersection.
+        // Draw the rays and the barriers.
         private void Timer1_Tick(object sender, EventArgs e)
         {
             gfx.Clear(Color.Black);
 
             mouse = pictureBox1.PointToClient(Cursor.Position);
 
-            for (currentAngle = 0; currentAngle < end; currentAngle += step)
+            for (currentAngle = 0; currentAngle < end; currentAngle += angleStep)
             {
                 ray = new Line(mouse.X, mouse.Y, mouse.X + (int)(Math.Cos(currentAngle) * rayScaleDist), mouse.Y + (int)(Math.Sin(currentAngle) * rayScaleDist));
                 minIntersectionDistance = int.MaxValue;
@@ -92,12 +96,13 @@ namespace Raycast
             pictureBox1.Image = canvas;
         }
 
+        // Found at https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
         private bool LineIntersection(Line ray, Line barrier, out Point intersectionPoint)
         {
             intersectionPoint = default;
-            double den = ((ray.X1 - ray.X2) * (barrier.Y1 - barrier.Y2)) - ((ray.Y1 - ray.Y2) * (barrier.X1 - barrier.X2));
-            double t = ((ray.X1 - barrier.X1) * (barrier.Y1 - barrier.Y2)) - ((ray.Y1 - barrier.Y1) * (barrier.X1 - barrier.X2));
-            double u = ((ray.X1 - ray.X2) * (ray.Y1 - barrier.Y1)) - ((ray.Y1 - ray.Y2) * (ray.X1 - barrier.X1));
+            int den = ((ray.X1 - ray.X2) * (barrier.Y1 - barrier.Y2)) - ((ray.Y1 - ray.Y2) * (barrier.X1 - barrier.X2));
+            float t = ((ray.X1 - barrier.X1) * (barrier.Y1 - barrier.Y2)) - ((ray.Y1 - barrier.Y1) * (barrier.X1 - barrier.X2));
+            float u = ((ray.X1 - ray.X2) * (ray.Y1 - barrier.Y1)) - ((ray.Y1 - ray.Y2) * (ray.X1 - barrier.X1));
 
             if (den == 0)
             {
@@ -116,16 +121,14 @@ namespace Raycast
             return false;
         }
 
+        // Distance formula.
         private int DistanceSquared(in int x1, in int y1, in int x2, in int y2)
         {
-            //var diffx = Math.Pow(x2 - x1, 2);
-            //var diffy = Math.Pow(y2 - y1, 2);
-            //return Math.Sqrt(diffx + diffy);
-
             int diffx = (x2 - x1) * (x2 - x1);
             int diffy = (y2 - y1) * (y2 - y1);
 
-            return diffx + diffy;   // compared against a larger number and other dist values, so we can forgo not using Math.Sqrt
+            // Compared against a larger number and other dist values, so we can forgo not using Math.Sqrt.
+            return diffx + diffy;
         }
     }
 }
